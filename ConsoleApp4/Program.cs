4abed4234da6f5592cc1801a68a5835096ac7ef9
Koko3000onsy <onsykoko79@gmail.com>
2026-02-10 01:01:43 +0200

    Q1

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace ConsoleApp4
+﻿using ConsoleApp4.enums;
+
+namespace ConsoleApp4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1 
+            Console.Write("enter the number of day of week\n");
+            int input =int .Parse(Console.ReadLine());
+            daysofweek day = (daysofweek)input;
+            Console.WriteLine($"the day of week is {day}");
+            switch (day) 
+            {
+                case daysofweek.saturday:
+                case daysofweek.sunday:
+                    Console.WriteLine("it is weekend");
+                    break;
+                default:
+                    Console.WriteLine("it is workday");
+                    break;
+            }
+            #endregion
+
+
+
         }
     }
 }
---
+++ ConsoleApp4/enums/daysofweek.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4.enums
+{
+    enum daysofweek
+    {
+        saturday,
+        sunday,
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday
+    }
+}
