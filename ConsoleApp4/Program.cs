48049b22945c6324e543d6aa050ed6fc01c94fe7
Koko3000onsy <onsykoko79@gmail.com>
2026-02-10 03:14:00 +0200

    Q2

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -7,20 +7,57 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
             #region Q1 
-            Console.Write("enter the number of day of week\n");
-            int input =int .Parse(Console.ReadLine());
-            daysofweek day = (daysofweek)input;
-            Console.WriteLine($"the day of week is {day}");
-            switch (day) 
-            {
-                case daysofweek.saturday:
-                case daysofweek.sunday:
-                    Console.WriteLine("it is weekend");
-                    break;
-                default:
-                    Console.WriteLine("it is workday");
-                    break;
-            }
+            //Console.Write("enter the number of day of week\n");
+            //int input =int .Parse(Console.ReadLine());
+            //daysofweek day = (daysofweek)input;
+            //Console.WriteLine($"the day of week is {day}");
+            //switch (day) 
+            //{
+            //    case daysofweek.saturday:
+            //    case daysofweek.sunday:
+            //        Console.WriteLine("it is weekend");
+            //        break;
+            //    default:
+            //        Console.WriteLine("it is workday");
+            //        break;
+            //}
+            #endregion
+            #region ََQ2
+            
+            //Console.Write("enter the size of array");
+            //int size=int.Parse(Console.ReadLine());
+            //int[] number = new int[size];
+            //for (int i = 0; i < size; i++)
+            //{
+            //    Console.Write($"enter element {i + 1} : ");
+            //    number[i] = int.Parse(Console.ReadLine());
+            //}
+            //int sum = 0;
+            //double avg = 0;
+            //int max = number[0];
+            //int min = number[0];
+            //for (int i= 0;i  < size;i ++)
+            //{
+            //    sum += number[i];
+            //        if(number[i] > max)
+            //        {
+            //            max = number[i];
+            //        }
+            //        if (number[i] < min)
+            //        {
+            //            min = number[i];
+            //        }
+            //}
+            //avg = sum / size;
+            //Console.WriteLine($"the sum of array is {sum}");
+            //Console.WriteLine($"the average of array is {avg}");
+            //Console.WriteLine($"the max of array is {max}");
+            //Console.WriteLine($"the min of array is {min}");
+            //for(int i=size-1;i >= 0;i--)
+            //{
+            //    Console.Write($"{number[i]} ");
+            //}
+
             #endregion
 
 
